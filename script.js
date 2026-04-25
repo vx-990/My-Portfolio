db19c5bea77920db095f741ff652211d02c36201
vx-990 <frankswearingen580@gmail.com>
2026-04-25 07:32:30 -0500

    Implement music toggle feature in script.js
    Add music toggle functionality with play/pause button

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Portfolio Loaded 🚀");
+const music = document.getElementById("bgMusic");
+const btn = document.getElementById("musicBtn");
+let playing = false;
+
+function toggleMusic(){
+  if(!playing){
+    music.volume = 0.4;
+
+    music.play()
+      .then(() => {
+        btn.innerHTML = "🔊";
+        playing = true;
+      })
+      .catch(err => {
+        console.log("Audio blocked:", err);
+        alert("Click the page first, then press the music button again.");
+      });
+
+  } else {
+    music.pause();
+    btn.innerHTML = "🔇";
+    playing = false;
+  }
+}
